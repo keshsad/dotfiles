98f7b1d9f89be41bc0bf5c973694427ef088f794
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2024-10-13 15:15:50 -0500

    added Git group

--- init.lua
+++ init.lua
@@ -315,6 +315,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>g', group = '[G]it' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
     end,
